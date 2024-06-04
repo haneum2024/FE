0e2168821f3b8b7117fac597616e345cea2a7604
Changhyun-Hong <alex963@naver.com>
2024-06-04 23:14:15 +0900

    sprint2 feat: firebase login setting and package download

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,11 @@
-const {getDefaultConfig} = require('metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-module.exports = (async () => {
-  const defaultConfig = await getDefaultConfig();
-  const {assetExts} = defaultConfig.resolver;
-  return {
-    resolver: {
-      // Add bin to assetExts
-      assetExts: [...assetExts, 'bin'],
-    },
-  };
-})();
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
