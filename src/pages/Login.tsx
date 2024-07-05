b635757de78d1fe1ed8da6591b00016c0762f3e5
Changhyun-Hong <alex963@naver.com>
2024-07-06 03:58:59 +0900

    sprint2 feat: 로그인 배포 주소 연결

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import {View, Pressable, StyleSheet, Text} from 'react-native';
-// import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 import {useDispatch} from 'react-redux';
-import {login} from '../store/reducers/authReducer';
 import {StackNavigationProp} from '@react-navigation/stack';
-import {PageNavigation} from '../../types/navigation';
 import {authorize} from 'react-native-app-auth';
-import {loginApi} from '../api/api';
 // import NaverLogin, {
 //   NaverLoginResponse,
 //   GetProfileResponse,
 // } from '@react-native-seoul/naver-login';
 import pkceChallenge from 'react-native-pkce-challenge';
 
+import {loginApi} from '../api/api';
+import {PageNavigation} from '../../types/navigation';
+import {setAccessToken} from '../storage/auth';
+import {login} from '../store/reducers/authReducer';
+
 interface LoginProps {
   navigation: StackNavigationProp<PageNavigation, 'SignUpFirstStep'>;
 }
 
-// 구글 클라우드 콘솔에서 얻은 안드로이드 클라이언트 ID
+// 구글 클라우드 콘솔에서 얻은 웹 클라이언트 ID
 const GOOGLE_CLIENT_ID =
-  '54570271712-a2ct0tbftq1gdrnf7p9pk6m8kfs4sbsl.apps.googleusercontent.com';
-const GOOGLE_REDIRECT_URI = 'com.mypet:/oauth2redirect/google';
+  // '54570271712-a2ct0tbftq1gdrnf7p9pk6m8kfs4sbsl.apps.googleusercontent.com';
+  '54570271712-6cj9qpfet066g5f0tb8le4hlvovr9073.apps.googleusercontent.com';
+// const GOOGLE_REDIRECT_URI = 'com.mypet:/oauth2redirect/google';
+// const GOOGLE_REDIRECT_URI = `com.googleusercontent.apps.${GOOGLE_CLIENT_ID}:/oauthredirect`;
+const GOOGLE_REDIRECT_URI =
+  'https://happy-maru.net/oauth2/authorization/google';
+const GOOGLE_CLIENT_SECRET = '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';
 
 // 네이버 클라이언트 정보
 const NAVER_CLIENT_ID = 'aRtsZ5tiPboW41G3a8iO';
@@ -30,8 +37,14 @@ const NAVER_REDIRECT_URI = 'com.mypet:/oauth2redirect/naver';
 const googleConfig = {
   issuer: 'https://accounts.google.com',
   clientId: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
   redirectUrl: GOOGLE_REDIRECT_URI,
   scopes: ['openid', 'profile', 'email'],
+  // usePKCE: true,
+  // serviceConfiguration: {
+  //   authorizationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
+  //   tokenEndpoint: 'https://oauth2.googleapis.com/token',
+  // },
 };
 
 const naverConfig = {
@@ -53,22 +66,32 @@ const Login: React.FC<LoginProps> = ({navigation}) => {
 
       const result = await authorize({
         ...googleConfig,
-        // additionalParameters: {
+        // additionalHeaders: {
         //   code_challenge: pkce.codeChallenge,
         //   code_challenge_method: 'S256',
         // },
       });
 
+      // GoogleSignin.configure({
+      //   webClientId: GOOGLE_CLIENT_ID,
+      //   offlineAccess: true,
+      // });
+
+      // await GoogleSignin.hasPlayServices();
+      // const userInfo = await GoogleSignin.signIn();
+      // console.log('User Info:', userInfo);
+
+      await setAccessToken(result.accessToken);
       console.log('Google Authorization result:', result);
 
       // 서버로 code_verifier 및 result.accessToken을 보내서 JWT 발급
-      const response = await loginApi({
-        provider: 'google',
-        code: result.accessToken,
-        codeVerifier: pkce.codeVerifier,
-      });
+      // const response = await loginApi({
+      //   provider: 'google',
+      //   code: result.accessToken,
+      //   codeVerifier: pkce.codeVerifier,
+      // });
 
-      console.log('Server response:', response.data);
+      // console.log('Server response:', response.data);
 
       dispatch(login());
     } catch (error) {
---
+++ src/storage/auth.ts
@@ -0,0 +1,27 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const getAccessToken = async () => {
+  try {
+    const accessToken = await AsyncStorage.getItem('accessToken');
+    return accessToken;
+  } catch (error) {
+    console.error('Failed to fetch access token from AsyncStorage', error);
+    return null;
+  }
+};
+
+export const setAccessToken = async (accessToken: string) => {
+  try {
+    await AsyncStorage.setItem('accessToken', accessToken);
+  } catch (error) {
+    console.error('Failed to save access token to AsyncStorage', error);
+  }
+};
+
+export const removeAccessToken = async () => {
+  try {
+    await AsyncStorage.removeItem('accessToken');
+  } catch (error) {
+    console.error('Failed to remove access token from AsyncStorage', error);
+  }
+};
