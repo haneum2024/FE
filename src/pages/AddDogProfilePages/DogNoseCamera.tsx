e6c3c3e84ad624c927425f2be9f3f1c16c4641d3
Jake Yubin KIM <jakepro657@gmail.com>
2024-07-27 22:10:20 +0900

    feat: dog nose camera to track dog's nose

--- src/pages/AddDogProfilePages/DogNoseCamera.tsx
+++ src/pages/AddDogProfilePages/DogNoseCamera.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RouteProp} from '@react-navigation/native';
-import {Button} from 'react-native-paper';
-
+import React, {useEffect, useRef, useState} from 'react';
 import CustomText from '../../components/CustomText';
 import type {AddDogPageNavigation} from '../../../types/navigation';
+import {StyleSheet, View} from 'react-native';
+import {Button, Text} from 'react-native-paper';
+import {RNCamera} from 'react-native-camera';
+import '@tensorflow/tfjs-react-native';
+import {useTensorflowModel} from 'react-native-fast-tflite';
+import * as tf from '@tensorflow/tfjs';
 
 interface DogNoseCameraType {
   navigation: StackNavigationProp<AddDogPageNavigation, 'DogNoseCamera'>;
@@ -13,6 +16,38 @@ interface DogNoseCameraType {
 }
 
 const DogNoseCamera = ({navigation, route}: DogNoseCameraType) => {
+  const tfModel = useTensorflowModel(
+    require('../../assets/yolov5su_saved_model/yolov5su_float32.tflite'),
+  );
+  const [predictions, setPredictions] = useState<any>(null);
+  const cameraRef = useRef<any | null>(null);
+
+  const runInference = async (input: any) => {
+    if (tfModel.state !== 'loaded' || tf.model === undefined) {
+      return null;
+    }
+    const output = tfModel.model.runSync([input]);
+    setPredictions(output);
+  };
+
+  const handleCameraStream = async (images: any) => {
+    const loop = async () => {
+      const nextImageTensor = images.next().value;
+      if (nextImageTensor) {
+        await runInference(nextImageTensor);
+        tf.dispose([nextImageTensor]);
+      }
+      requestAnimationFrame(loop);
+    };
+    loop();
+  };
+
+  useEffect(() => {
+    (async () => {
+      await tf.ready();
+    })();
+  }, []);
+
   const {
     dogName,
     dogBreed,
@@ -29,6 +64,19 @@ const DogNoseCamera = ({navigation, route}: DogNoseCameraType) => {
 
   return (
     <View style={styles.cameraContainer}>
+      <View style={styles.container}>
+        <RNCamera
+          style={styles.camera}
+          type={RNCamera.Constants.Type.back}
+          onCameraReady={() => {
+            if (cameraRef.current) {
+              const images = cameraRef.current.getPreviewData();
+              handleCameraStream(images);
+            }
+          }}
+        />
+        {predictions && <Text>{JSON.stringify(predictions)}</Text>}
+      </View>
       <Text>Dog Name: {dogName}</Text>
       <Text>Dog Breed: {dogBreed}</Text>
       <Text>Dog Gender: {dogGender}</Text>
@@ -65,6 +113,15 @@ const DogNoseCamera = ({navigation, route}: DogNoseCameraType) => {
 const styles = StyleSheet.create({
   cameraContainer: {
     padding: 20,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  camera: {
+    flex: 1,
+    width: '100%',
   },
 });
 
