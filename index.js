312ad5826d2ae3f2c9886e033586e6ce1812d696
easyhak <jooin2000@cau.ac.kr>
2024-08-19 02:55:28 +0900

    Feat: background alarm test

--- index.js
+++ index.js
@@ -2,10 +2,16 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import {Alert, AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import './global.js';
 import 'react-native-get-random-values';
+import messaging from "@react-native-firebase/messaging";
+
+messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+    console.log('Message handled in the background!', remoteMessage);
+    Alert.alert("good")
+});
 
 AppRegistry.registerComponent(appName, () => App);
